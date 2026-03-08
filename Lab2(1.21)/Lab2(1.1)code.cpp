ff89007487a28c2c5822bacd8f068a4ea8f066e1
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-08 13:37:09 +0200

    Add files via upload

---
+++ Lab2(1.21)/Lab2(1.1)code.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h> 
+#include <locale.h> 
+#include <math.h>
+int main() {
+	setlocale(LC_ALL, "Ukrainian");
+	int a;
+	int b;
+	printf("Ведіть 1 число:");
+	scanf_s("%d", &a);
+	printf("Ведіть 2 число:");
+	scanf_s("%d", &b);
+	if (a != 0 && b != 0) {
+		printf("Ділення другого числа на перше, залишок: %d\n", b % a);
+		printf("Ділення першого числа на друге: %d\n", a / b);
+		printf("Множення двох чисел: %d\n", a * b);
+	}
+}
