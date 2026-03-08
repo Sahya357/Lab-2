207ead53d6e891c6ecb2f116f3b452c602c747c1
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-08 13:36:35 +0200

    Add files via upload

---
+++ Lab2(3.21)/Lab2(3.21)code.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h> 
+#include <locale.h> 
+#include <math.h>
+int main() {
+	setlocale(LC_ALL, "Ukrainian");
+	int x;
+	int y;
+	int z;
+	int a;
+	scanf_s("%d %d %d", &x, &y, &z);
+	if (x != 0 && y != 0 && z != 0) {
+		a = ((sqrt(x) + 2 * sqrt(y * z)) / (sqrt(y) - 2 * sqrt(x * z)));
+		printf("Відповідь : %d",a);
+	}
+
+}
