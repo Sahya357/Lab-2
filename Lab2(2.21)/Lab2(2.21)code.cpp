e9f279c1cc3edff30f59cadeda39056ff1dc9e90
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-16 18:59:45 +0200

    Change variables to constants in Lab2 code

--- Lab2(2.21)/Lab2(2.21)code.cpp
+++ Lab2(2.21)/Lab2(2.21)code.cpp
@@ -1,11 +1,11 @@
-﻿#include <stdio.h> 
+#include <stdio.h> 
 #include <locale.h> 
 #include <math.h>
 int main() {
 	setlocale(LC_ALL, "Ukrainian");
-	int a = 4;
-	int b = -2;
-	int c = 20;
+	const int a = 4;
+	const int b = -2;
+	const int c = 20;
 	int y = (((a * b * c) / a + b + c) - ((a - b - c) / (a*b*c)));
 	printf("значенням функції є : %d", y);
 
