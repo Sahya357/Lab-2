51027b77a3c5669c1a5edac0399a70508d55b287
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-16 18:58:57 +0200

    Refactor main function for cleaner input handling

--- Lab2(1.21)/Lab2(1.1)/Lab2(1.1).cpp
+++ Lab2(1.21)/Lab2(1.1)/Lab2(1.1).cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h> 
+#include <stdio.h> 
 #include <locale.h> 
 #include <math.h>
 int main() {
@@ -9,9 +9,9 @@ int main() {
 	scanf_s("%d", &a);
 	printf("Ведіть 2 число:");
 	scanf_s("%d", &b);
-	if (a != 0 && b != 0) {
+	
 		printf("Ділення другого числа на перше, залишок: %d\n", b % a);
 		printf("Ділення першого числа на друге: %d\n", a / b);
 		printf("Множення двох чисел: %d\n", a * b);
-	}
+	
 }
